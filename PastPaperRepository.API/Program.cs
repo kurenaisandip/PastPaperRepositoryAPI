dd366d9dcefc51dbcff1f71b3c6474bea2f0df79
Sandip <srai68141@gmail.com>
2024-10-08 12:28:05 +0545

    add repository to the api layer

--- PastPaperRepository.API/Program.cs
+++ PastPaperRepository.API/Program.cs
@@ -1,9 +1,15 @@
+using PastPaperRepository.Application.ApplicationService;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// not a good way to DI in the application
+// builder.Services.AddSingleton<IPastPaperRepository, PastPaperRepository>();
+builder.Services.AddApplication();
 
 var app = builder.Build();
 
