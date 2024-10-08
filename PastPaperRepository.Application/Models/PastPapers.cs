2f0e2fc3ae319e6ae9bbb9b9ed55b7c9b3de2f9b
Sandip <srai68141@gmail.com>
2024-10-08 12:04:31 +0545

    feat: added fields to structure the model for crud operation

---
+++ PastPaperRepository.Application/Models/PastPapers.cs
@@ -0,0 +1,14 @@
+﻿namespace PastPaperRepository.Application.Models;
+
+public class PastPapers
+{
+    public Guid PastPaperId { get; init; }
+    public string Title { get; set; }
+    public int SubjectId { get; set; }
+    public int CategoryId { get; set; }
+    public int Year { get; set; }
+    public string ExamType { get; set; }
+    public string DifficultyLevel { get; set; }
+    public string ExamBoard { get; set; }
+    public string FilePath { get; set; }
+}
