0c660d4b6283d10981a2ca2b57bdfacc0ec00722
Sandip <srai68141@gmail.com>
2024-10-08 11:56:33 +0545

    feat: added class file to handle the response for list of past papers

---
+++ PastPaperRepository.Contracts/Responses/PastPapersResponse.cs
@@ -0,0 +1,6 @@
+﻿namespace PastPaperRepository.Contracts.Responses;
+
+public class PastPapersResponse
+{
+    public IEnumerable<PastPaperResponse> PastPapers { get; init; } = Enumerable.Empty<PastPaperResponse>();
+}
