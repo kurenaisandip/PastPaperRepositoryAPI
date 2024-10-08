618b8d3024b4e9d2a1b3006c694179cb0ec79e93
Sandip <srai68141@gmail.com>
2024-10-08 12:27:50 +0545

    created the extension method to pass the DI in the program.cs

--- PastPaperRepository.Application/ApplicationService/ApplicationServiceCollectionExtensions.cs
+++ PastPaperRepository.Application/ApplicationService/ApplicationServiceCollectionExtensions.cs
@@ -1,6 +1,15 @@
-﻿namespace PastPaperRepository.Application.ApplicationService;
+﻿using Microsoft.Extensions.DependencyInjection;
+using PastPaperRepository.Application.Repository;
+
+namespace PastPaperRepository.Application.ApplicationService;
 
 public static class ApplicationServiceCollectionExtensions
 {
+
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddSingleton<IPastPaperRepository, Repository.PastPaperRepository>();
+        return services;
+    }
     
 }
