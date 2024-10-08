0f01e584f0609d407c5dd5aea5855a0e5c9661f0
Sandip <srai68141@gmail.com>
2024-10-08 11:53:28 +0545

    feat: added request for the update past papers

---
+++ PastPaperRepository.Contracts/Requests/UpdatePastPaperRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace PastPaperRepository.Contracts.Requests;
+
+public class UpdatePastPaperRequest
+{
+    public string Title { get; init; }
+    public int SubjectId { get; init; }
+    public int CategoryId { get; init; }
+    public int Year { get; init; }
+    public string ExamType { get; init; }
+    public string DifficultyLevel { get; init; }
+    public string ExamBoard { get; init; }
+    public string FilePath { get; init; }
+}
