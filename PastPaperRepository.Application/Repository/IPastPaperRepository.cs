868dc08a3e968565e97e5fef6b8e6b9e3615009d
Sandip <srai68141@gmail.com>
2024-10-08 12:04:56 +0545

    added methods for the crud operation to be implemented in the class file

--- PastPaperRepository.Application/Repository/IPastPaperRepository.cs
+++ PastPaperRepository.Application/Repository/IPastPaperRepository.cs
@@ -1,6 +1,14 @@
-﻿namespace PastPaperRepository.Application.Repository;
+﻿using PastPaperRepository.Application.Models;
+
+namespace PastPaperRepository.Application.Repository;
 
 public interface IPastPaperRepository
 {
+    Task<bool> CreatePaspaperAsync(PastPapers pastPapers);
+    Task<PastPapers?> GetPastPaperByIdAsync(Guid pastPaperId);
     
+    Task<IEnumerable<PastPapers>> GetAllPastPapersAsync();
+    
+    Task<bool> UpdatePastPaperAsync(PastPapers pastPapers);
+    Task<bool> DeletePastPaperAsync(Guid pastPaperId);
 }
