39cfe2dd65924a4dfbe337353a4649af327bd949
Sandip <srai68141@gmail.com>
2024-10-08 12:05:29 +0545

    feat: implemented the interface in the class

--- PastPaperRepository.Application/Repository/PastPaperRepository.cs
+++ PastPaperRepository.Application/Repository/PastPaperRepository.cs
@@ -1,6 +1,31 @@
-﻿namespace PastPaperRepository.Application.Repository;
+﻿using PastPaperRepository.Application.Models;
+
+namespace PastPaperRepository.Application.Repository;
 
 public class PastPaperRepository : IPastPaperRepository
 {
-    
+    public Task<bool> CreatePaspaperAsync(PastPapers pastPapers)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<PastPapers?> GetPastPaperByIdAsync(Guid pastPaperId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<PastPapers>> GetAllPastPapersAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> UpdatePastPaperAsync(PastPapers pastPapers)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> DeletePastPaperAsync(Guid pastPaperId)
+    {
+        throw new NotImplementedException();
+    }
 }
