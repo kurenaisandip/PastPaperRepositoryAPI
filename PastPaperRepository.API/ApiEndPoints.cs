3c264ddf552d0e1f4a6275bcb263ed82c86da621
Sandip <srai68141@gmail.com>
2024-10-15 11:33:04 +0545

    feat: added a base url for the api and created a class which returns the string of the create url

---
+++ PastPaperRepository.API/ApiEndPoints.cs
@@ -0,0 +1,14 @@
+﻿namespace PastPaperRepository.API;
+
+public static class ApiEndPoints
+{
+    private const string ApiBaseUrl = "api";
+    
+    public static class PastPaper
+    {
+     private const string Base = $"{ApiBaseUrl}/createpastpaper";
+
+     public const string Create = Base;
+
+    }
+}
