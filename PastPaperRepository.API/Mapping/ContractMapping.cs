5053590a05e2ebe74db05ced0fb31efc849664c1
Sandip <srai68141@gmail.com>
2024-10-15 11:24:18 +0545

    feat: created mapping for both cretaing the pastpaper and also the reponse

---
+++ PastPaperRepository.API/Mapping/ContractMapping.cs
@@ -0,0 +1,41 @@
+﻿using PastPaperRepository.Application.Models;
+using PastPaperRepository.Contracts.Requests;
+using PastPaperRepository.Contracts.Responses;
+
+namespace PastPaperRepository.API.Mapping;
+
+public static class ContractMapping
+{
+    public static PastPapers MapToPastPapers(this CreatePastPaperRequest request)
+    {
+        return new PastPapers()
+        {
+            PastPaperId = Guid.NewGuid(),
+            Title = request.Title,
+            SubjectId = request.SubjectId,
+            CategoryId = request.CategoryId,
+            Year = request.Year,
+            ExamType = request.ExamType,
+            DifficultyLevel = request.DifficultyLevel,
+            ExamBoard = request.ExamBoard,
+            FilePath = request.FilePath
+        };
+    }
+
+    public static PastPaperResponse MapToResponsePastPaper(this PastPapers pastPaper)
+    {
+        return new PastPaperResponse()
+        {
+            PastPaperId = pastPaper.PastPaperId,
+            Title = pastPaper.Title,
+            SubjectId = pastPaper.SubjectId,
+            CategoryId = pastPaper.CategoryId,
+            Year = pastPaper.Year,
+            ExamType = pastPaper.ExamType,
+            DifficultyLevel = pastPaper.DifficultyLevel,
+            ExamBoard = pastPaper.ExamBoard,
+            FilePath = pastPaper.FilePath
+        };
+
+    }
+}
