77ab708190bbc190973a8c576a9e205e814f5c8b
Sandip <srai68141@gmail.com>
2024-10-19 15:53:52 +0545

    feat: response for updated past paper

---
+++ PastPaperRepository.Contracts/Responses/UpdatePastPaperResponse.cs
@@ -0,0 +1,15 @@
+﻿namespace PastPaperRepository.Contracts.Responses;
+
+public class UpdatePastPaperResponse
+{
+        public Guid PastPaperId { get; init; }
+        public string Title { get; init; }
+        public int SubjectId { get; init; }
+        public int CategoryId { get; init; }
+        public int Year { get; init; }
+        public string ExamType { get; init; }
+        public string DifficultyLevel { get; init; }
+        public string ExamBoard { get; init; }
+        public string FilePath { get; init; }
+    
+}
