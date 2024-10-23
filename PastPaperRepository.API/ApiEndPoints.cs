6bf3d44bbdae1a387c54750b3135f952d9fbe2c4
Sandip <srai68141@gmail.com>
2024-10-23 16:36:20 +0545

    feat: added endpoints for getbyslug

--- PastPaperRepository.API/ApiEndPoints.cs
+++ PastPaperRepository.API/ApiEndPoints.cs
@@ -10,6 +10,7 @@ public static class ApiEndPoints
 
      public const string Create = Base;
      public const string Get = $"{Base}/{{id:guid}}";
+     public const string GetBySlug = $"{Base}/{{idOrSlug}}";
      public const string GetAll = $"{Base}/all";
      public const string Update = $"{Base}/{{id:guid}}";
      public const string Delete = $"{Base}/{{id:guid}}";
