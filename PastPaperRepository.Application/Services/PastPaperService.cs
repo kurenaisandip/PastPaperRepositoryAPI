9a0a6614c140200dbc694ff7974c9142e3eb50e1
Sandip <srai68141@gmail.com>
2024-10-26 22:43:57 +0545

    added validators in the service class to do some validation in the create and update method and throw any error if there is problem and catch the errors in the api

--- PastPaperRepository.Application/Services/PastPaperService.cs
+++ PastPaperRepository.Application/Services/PastPaperService.cs
@@ -1,20 +1,25 @@
-﻿using PastPaperRepository.Application.Models;
+﻿using FluentValidation;
+using PastPaperRepository.Application.Models;
 using PastPaperRepository.Application.Repositories;
+using PastPaperRepository.Application.Validators;
 
 namespace PastPaperRepository.Application.Services;
 
 public class PastPaperService: IPastPaperService
 {
     private readonly IPastPaperRepository _pastPaperRepository;
+    private readonly IValidator<PastPapers> _pastPaperValidators;
 
-    public PastPaperService(IPastPaperRepository pastPaperRepository)
+    public PastPaperService(IPastPaperRepository pastPaperRepository, IValidator<PastPapers> pastPaperValidators)
     {
         _pastPaperRepository = pastPaperRepository;
+        _pastPaperValidators = pastPaperValidators;
     }
 
-    public Task<bool> CreatePastPaperAsync(PastPapers pastPapers)
+    public async Task<bool> CreatePastPaperAsync(PastPapers pastPapers)
     {
-        return _pastPaperRepository.CreatePastPaperAsync(pastPapers);
+        await _pastPaperValidators.ValidateAndThrowAsync(pastPapers);
+        return await _pastPaperRepository.CreatePastPaperAsync(pastPapers);
     }
 
     public Task<PastPapers?> GetPastPaperByIdAsync(string pastPaperId)
@@ -34,6 +39,7 @@ public class PastPaperService: IPastPaperService
 
     public async Task<PastPapers?> UpdatePastPaperAsync(PastPapers pastPapers)
     {
+        await _pastPaperValidators.ValidateAndThrowAsync(pastPapers);
       var pastPaperExists = await _pastPaperRepository.ExistsById(pastPapers.PastPaperId);
       if (!pastPaperExists)
       {
