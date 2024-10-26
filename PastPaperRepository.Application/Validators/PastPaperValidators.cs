e6ab55eab9df582eac4dadd2a3622da2e2b56691
Sandip <srai68141@gmail.com>
2024-10-26 22:43:03 +0545

    feat: inject service into validation to do some logic in the future

--- PastPaperRepository.Application/Validators/PastPaperValidators.cs
+++ PastPaperRepository.Application/Validators/PastPaperValidators.cs
@@ -1,12 +1,15 @@
 ﻿using FluentValidation;
 using PastPaperRepository.Application.Models;
+using PastPaperRepository.Application.Repositories;
 
 namespace PastPaperRepository.Application.Validators;
 
 public class PastPaperValidators : AbstractValidator<PastPapers>
 {
-    public PastPaperValidators()
+    private readonly IPastPaperRepository _pastPaperRepository;
+    public PastPaperValidators(IPastPaperRepository pastPaperRepository)
     {
+        _pastPaperRepository = pastPaperRepository;
         RuleFor(x => x.PastPaperId).NotEmpty();
         RuleFor(x => x.Title).NotEmpty();
         RuleFor(x => x.Year).LessThanOrEqualTo(DateTime.UtcNow.Year);
