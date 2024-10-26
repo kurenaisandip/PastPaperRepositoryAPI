405daedafb59aaa4c3b70aa726f4f24e6f1595d5
Sandip <srai68141@gmail.com>
2024-10-26 22:29:07 +0545

    feat: added a validator class to validate the business layer in the api with the use of fluent validation package from nuget

---
+++ PastPaperRepository.Application/Validators/PastPaperValidators.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using PastPaperRepository.Application.Models;
+
+namespace PastPaperRepository.Application.Validators;
+
+public class PastPaperValidators : AbstractValidator<PastPapers>
+{
+    public PastPaperValidators()
+    {
+        RuleFor(x => x.PastPaperId).NotEmpty();
+        RuleFor(x => x.Title).NotEmpty();
+        RuleFor(x => x.Year).LessThanOrEqualTo(DateTime.UtcNow.Year);
+        RuleFor(x => x.SubjectId).GreaterThan(0);
+        RuleFor(x => x.CategoryId).GreaterThan(0);
+        RuleFor(x => x.ExamBoard).NotEmpty();
+        RuleFor(x => x.DifficultyLevel).NotEmpty();
+        RuleFor(x => x.ExamType).NotEmpty();
+        RuleFor(x => x.FilePath).NotEmpty();
+
+    }
+}
