6ba2ec2599c2ecba057ce8cd64778a811fe72b25
Sandip <srai68141@gmail.com>
2024-10-26 22:53:41 +0545

    created a middleware to catch the error from the service and pass the context if sucessful and if not then throw the error with 400 status code

---
+++ PastPaperRepository.API/Mapping/ValidationMappingMiddleware.cs
@@ -0,0 +1,37 @@
+﻿using FluentValidation;
+using PastPaperRepository.Contracts.Responses;
+
+namespace PastPaperRepository.API.Mapping;
+
+public class ValidationMappingMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public ValidationMappingMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task HandleAsync(HttpContext context)
+    {
+        try
+        {
+            //pass the context to the next middleware if sucessfull
+            await _next(context);
+        }
+        catch (ValidationException exception)
+        {
+            context.Response.StatusCode = 400; //bad request
+            var validationFailureResponse = new ValidationFailureResponse
+            {
+                Errors = exception.Errors.Select(x => new ValidationResponse
+                {
+                    PropertyName = x.PropertyName,
+                    Message = x.ErrorMessage
+                })
+            };
+            
+            await context.Response.WriteAsJsonAsync(validationFailureResponse);
+        }
+    }
+}
