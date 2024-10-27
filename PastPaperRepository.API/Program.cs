a0106a763fffc00b71b26712fe8d7b78a4cedf6d
Sandip <srai68141@gmail.com>
2024-10-27 21:34:26 +0545

    feat: added authenticationand authorization based on the jwt token inthe controller and Program.cs

--- PastPaperRepository.API/Program.cs
+++ PastPaperRepository.API/Program.cs
@@ -50,6 +50,9 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
+app.UseAuthorization();
+
 app.UseMiddleware<ValidationMappingMiddleware>();
 
 app.MapControllers();
--- PastPaperRepository.Application/obj/Debug/net8.0/PastPaperRepository.Application.AssemblyInfo.cs
+++ PastPaperRepository.Application/obj/Debug/net8.0/PastPaperRepository.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PastPaperRepository.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+036caf051e6412c8b5c40e585c4e9315d324fa30")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6e63c6a62812eda55a8bde1293a794b5bc88581")]
 [assembly: System.Reflection.AssemblyProductAttribute("PastPaperRepository.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PastPaperRepository.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
