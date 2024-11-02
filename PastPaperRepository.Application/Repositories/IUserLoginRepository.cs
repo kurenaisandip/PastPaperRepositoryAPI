b5d8b9b792ee9a05e99ad4581054ee8f61615c28
Sandip <srai68141@gmail.com>
2024-11-02 10:13:31 +0545

    feat: added repository for the storing user and cheking if the user exist or not in in memory database

---
+++ PastPaperRepository.Application/Repositories/IUserLoginRepository.cs
@@ -0,0 +1,10 @@
+﻿using PastPaperRepository.Application.Models;
+
+namespace PastPaperRepository.Application.Repositories;
+
+public interface IUserLoginRepository
+{
+    Task<bool> Login(UsersLogin userLogin);
+    
+    Task<bool> Register(UsersLogin userLogin);
+}
---
+++ PastPaperRepository.Application/Repositories/UserLoginRepository.cs
@@ -0,0 +1,23 @@
+﻿using PastPaperRepository.Application.Models;
+
+namespace PastPaperRepository.Application.Repositories;
+
+public class UserLoginRepository: IUserLoginRepository
+{
+    private readonly List<UsersLogin> _users = new();
+    
+    public Task<bool> Login(UsersLogin userLogin)
+    {
+        if (_users.Any(x => x.Email == userLogin.Email && x.Password == userLogin.Password))
+        {
+            return Task.FromResult(true);
+        }
+        return Task.FromResult(false);
+    }
+
+    public Task<bool> Register(UsersLogin userLogin)
+    {
+       _users.Add(userLogin);
+         return Task.FromResult(true);
+    }
+}
