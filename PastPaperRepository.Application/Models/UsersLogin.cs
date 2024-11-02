d673e618a7df2bf7119f2672f79c80fdde778c87
Sandip <srai68141@gmail.com>
2024-11-02 10:12:08 +0545

    added model for userlogin

---
+++ PastPaperRepository.Application/Models/UsersLogin.cs
@@ -0,0 +1,8 @@
+﻿namespace PastPaperRepository.Application.Models;
+
+public class UsersLogin
+{
+    public string? Name { get; set; }
+    public required string Email { get; set; }
+    public required string Password { get; set; }
+}
