ef822fbf8980d59b82fb5a891d463fa8c804d3d6
Sandip <srai68141@gmail.com>
2024-11-02 09:40:43 +0545

    feat: created request contract for the login

---
+++ PastPaperRepository.Contracts/Requests/LogInUser.cs
@@ -0,0 +1,7 @@
+﻿namespace PastPaperRepository.Contracts.Requests;
+
+public class LogInUser
+{
+    public required string Email { get; init; }
+    public required string Password { get; init; }
+}
