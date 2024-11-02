5ceba9031579c1e1d4451ee786ce16048686aa68
Sandip <srai68141@gmail.com>
2024-11-02 19:20:34 +0545

    feat: added model for register user and removed name from the user model

--- PastPaperRepository.Contracts/Requests/LogInUser.cs
+++ PastPaperRepository.Contracts/Requests/LogInUser.cs
@@ -2,6 +2,12 @@
 
 public class LogInUser
 {
+    public required string Email { get; init; }
+    public required string Password { get; init; }
+}
+
+public class RegisterUser
+{
     public string Name { get; init; }
     public required string Email { get; init; }
     public required string Password { get; init; }
