6e72261e94baddf3cf27241134f1b36a8f0e2f55
Sandip <srai68141@gmail.com>
2024-11-02 20:27:42 +0545

    feat: added logic for checking if the user email and passsword matches for login operation

--- PastPaperRepository.Application/Repositories/UserLoginRepository.cs
+++ PastPaperRepository.Application/Repositories/UserLoginRepository.cs
@@ -1,4 +1,6 @@
-﻿using PastPaperRepository.Application.Database;
+﻿using System.Transactions;
+using Dapper;
+using PastPaperRepository.Application.Database;
 using PastPaperRepository.Application.Models;
 
 namespace PastPaperRepository.Application.Repositories;
@@ -12,9 +14,32 @@ public class UserLoginRepository: IUserLoginRepository
         _connectionFactory = connectionFactory;
     }
 
-    public Task<bool> Login(UsersLogin userLogin, CancellationToken token = default)
+    public async Task<bool> Login(UsersLogin userLogin, CancellationToken token = default)
     {
-       
+        using (var connection = await _connectionFactory.CreateConnectionAsync(token))
+        {
+            var transaction =  connection.BeginTransaction();
+            try
+            {
+                transaction.Commit();
+                var query = "SELECT COUNT(1) FROM Users WHERE Email = @Email AND Password = @Password";
+                var result = await connection.ExecuteScalarAsync<int>(new CommandDefinition(query, new { userLogin.Email, userLogin.Password }, transaction, cancellationToken: token));
+
+                if (result == 0)
+                {
+                    return false;
+                }
+
+                return result > 0;
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                transaction.Rollback();
+                throw;
+            }   
+        }
     }
 
     public Task<bool> Register(UsersLogin userLogin, CancellationToken token = default)
