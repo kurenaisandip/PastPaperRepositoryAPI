ea34ded87f606ddc7796fcac5566a272eed7fba8
Sandip <srai68141@gmail.com>
2024-11-03 20:40:19 +0545

    feat: added fields that is acceptable for sorting in the database

--- PastPaperRepository.Application/Validators/GetAllPastPapersOptionValidator.cs
+++ PastPaperRepository.Application/Validators/GetAllPastPapersOptionValidator.cs
@@ -5,8 +5,13 @@ namespace PastPaperRepository.Application.Validators;
 
 public class GetAllPastPapersOptionValidator: AbstractValidator<GetAllPastPapersOptions>
 {
+    private static readonly string[] AcceptableSortFields = {"Title", "Year", "DifficultyLevel", "ExamType", "ExamBoard"};
     public GetAllPastPapersOptionValidator()
     {
         RuleFor(x => x.Year).LessThanOrEqualTo(DateTime.UtcNow.Year);
+        
+        RuleFor(x => x.SortField)
+            .Must(field => AcceptableSortFields.Contains(field))
+            .WithMessage("Invalid sort field. Acceptable fields are: " + string.Join(", ", AcceptableSortFields));
     }
 }
