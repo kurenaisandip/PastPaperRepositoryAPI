8552823552a2b4f9d2ce92e101fafa7868a7f4a4
Sandip <srai68141@gmail.com>
2024-11-03 19:31:18 +0545

    feat: added code to get the user-id from the http context

---
+++ PastPaperRepository.API/Auth/IdentityExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace PastPaperRepository.API.Auth;
+
+public static class IdentityExtensions
+{
+ public static string? GetUserId(this HttpContext httpContext)
+ {
+     var userId =  httpContext.User.Claims.SingleOrDefault(x => x.Type == "userId");
+
+     if (userId is null )
+     {
+         return null;
+     }
+
+     return userId.ToString();
+ }
+}
