5519df2c95d31031e945056f99ce7cbf5b439f63
Sandip <srai68141@gmail.com>
2024-11-03 19:55:07 +0545

    feat: added validator for filtering the past papers based on year

---
+++ PastPaperRepository.Application/Validators/GetAllPastPapersOptionValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+using PastPaperRepository.Application.Models;
+
+namespace PastPaperRepository.Application.Validators;
+
+public class GetAllPastPapersOptionValidator: AbstractValidator<GetAllPastPapersOptions>
+{
+    public GetAllPastPapersOptionValidator()
+    {
+        RuleFor(x => x.Year).LessThanOrEqualTo(DateTime.UtcNow.Year);
+    }
+}
