de2db15dfbb7d3a473715c1be1beff0d7ea2de71
Sandip <srai68141@gmail.com>
2024-11-11 23:17:24 +0545

    feat: added response contract for role

---
+++ PastPaperRepository.Contracts/Responses/EducationalEntitiiesReponses/RoleResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace PastPaperRepository.Contracts.Responses.EducationalEntitiiesReponses;
+
+public class RoleResponse
+{
+    public int RoleId { get; init; }
+    
+    public string Name { get; init; }
+}
+
+public class RoleResponses
+{
+    public IEnumerable<RoleResponse> RolesAsEnumerable => Enumerable.Empty<RoleResponse>();
+}
