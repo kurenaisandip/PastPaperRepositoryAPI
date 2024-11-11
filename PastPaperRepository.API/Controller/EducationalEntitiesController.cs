174d2f3457bcfedce0c30002da17787b690680f2
Sandip <srai68141@gmail.com>
2024-11-11 23:42:19 +0545

    feat: added controller to take a request from a user and save the request in the db and send a reponse

--- PastPaperRepository.API/Controller/EducationalEntitiesController.cs
+++ PastPaperRepository.API/Controller/EducationalEntitiesController.cs
@@ -1,6 +1,29 @@
-﻿namespace PastPaperRepository.API.Controller;
+﻿using Microsoft.AspNetCore.Mvc;
+using PastPaperRepository.API.Mapping;
+using PastPaperRepository.Application.Repositories;
+using PastPaperRepository.Contracts.Requests.EducationalEntities;
 
-public class EducationalEntitiesController
+namespace PastPaperRepository.API.Controller;
+
+[ApiController]
+public class EducationalEntitiesController : ControllerBase
 {
-    
+    public readonly IEducationalEntitiesRepository _repository;
+
+    public EducationalEntitiesController(IEducationalEntitiesRepository repository)
+    {
+        _repository = repository;
+    }
+
+    [HttpPost(ApiEndPoints.EducationalEntities.CreateRole)]
+    public async Task<IActionResult> CreateRole([FromBody] CreateRoleRequest request)
+    {
+        var role = request.MapToRoles();
+        
+        await _repository.CreateRoleAsync(role);
+        
+        var response = role.MapToResponseRole();
+
+        return Ok(response);
+    }
 }
