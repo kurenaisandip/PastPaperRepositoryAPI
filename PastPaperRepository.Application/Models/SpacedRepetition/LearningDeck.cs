43c17ce9d0eae5f15556161a57d451f86caeb063
Sandip <srai68141@gmail.com>
2024-12-18 23:34:01 +0545

    feat: added model to save the data when user uses the spaced repetition

---
+++ PastPaperRepository.Application/Models/SpacedRepetition/LearningDeck.cs
@@ -0,0 +1,17 @@
+﻿namespace PastPaperRepository.Application.Models.SpacedRepetition;
+
+public class LearningDeck
+{
+    public long UserId { get; init; }
+    public string PastPaperId { get; init; }
+    public DateTime AddedDate { get; init; }
+    public DateTime NextReviewDate { get; init; }
+    public string Status { get; init; }
+
+    #region nullable properties
+
+    public long? QuestionNumber { get; init; }
+    public long? Score { get; init; }
+    
+    #endregion
+}
