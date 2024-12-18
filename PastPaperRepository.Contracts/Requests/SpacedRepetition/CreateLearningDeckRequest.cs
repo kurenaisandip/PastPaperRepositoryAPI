17af1f9bf8079c1bb3f03eb791e8c978bd088cd8
Sandip <srai68141@gmail.com>
2024-12-18 23:33:09 +0545

    refactor: added status property in the class

--- PastPaperRepository.Contracts/Requests/SpacedRepetition/CreateLearningDeckRequest.cs
+++ PastPaperRepository.Contracts/Requests/SpacedRepetition/CreateLearningDeckRequest.cs
@@ -4,6 +4,7 @@ public class CreateLearningDeckRequest
 {
     public long UserId { get; init; }
     public string PastPaperId { get; init; }
+    public string Status { get; init; }
     public DateTime AddedDate { get; init; }
     public DateTime NextReviewDate { get; init; }
 }
