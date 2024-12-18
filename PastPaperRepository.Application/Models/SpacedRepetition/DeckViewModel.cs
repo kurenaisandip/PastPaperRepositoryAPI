24652f884e1843b0bbcd10fa885c37d4c7c61076
Sandip <srai68141@gmail.com>
2024-12-18 23:33:32 +0545

    feat: added model to show the deck view to the user

---
+++ PastPaperRepository.Application/Models/SpacedRepetition/DeckViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace PastPaperRepository.Application.Models.SpacedRepetition;
+
+public class DeckViewModel
+{
+    public string pastPaperId { get; set; }
+    public string Title { get; set; }
+    public long Totalquestions { get; set; }
+}
