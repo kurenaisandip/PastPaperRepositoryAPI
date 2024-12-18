d3f70e193b4c895128b04af12abd1c5dca9535c7
Sandip <srai68141@gmail.com>
2024-12-18 22:09:35 +0545

    feat: created the request for making the learning deck for users

---
+++ PastPaperRepository.Contracts/Requests/SpacedRepetition/CreateLearningDeckRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace PastPaperRepository.Contracts.Requests.SpacedRepetition;
+
+public class CreateLearningDeckRequest
+{
+    public long UserId { get; init; }
+    public string PastPaperId { get; init; }
+    public DateTime AddedDate { get; init; }
+    public DateTime NextReviewDate { get; init; }
+}
