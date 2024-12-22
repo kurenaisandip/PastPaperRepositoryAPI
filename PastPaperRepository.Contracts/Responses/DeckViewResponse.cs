68f2bf20284e20ed65ae81e149b2ee30ade428e2
Sandip <srai68141@gmail.com>
2024-12-22 15:32:10 +0545

    feat: added response contract for deck view

---
+++ PastPaperRepository.Contracts/Responses/DeckViewResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace PastPaperRepository.Contracts.Responses;
+
+public class DeckViewResponse
+{
+    public string PastPaperId { get; set; } = default!;
+    public string Title { get; set; } = default!;
+    public long Totalquestions { get; set; } = default!;
+}
