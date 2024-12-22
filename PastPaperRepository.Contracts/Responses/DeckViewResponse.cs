a8fd45d5e238fb5d6253f9348bd2b71c1165dd4d
Sandip <srai68141@gmail.com>
2024-12-22 15:33:32 +0545

    refactor: changed the setter to init to limit any chnages in the data from other place

--- PastPaperRepository.Contracts/Responses/DeckViewResponse.cs
+++ PastPaperRepository.Contracts/Responses/DeckViewResponse.cs
@@ -2,7 +2,7 @@
 
 public class DeckViewResponse
 {
-    public string PastPaperId { get; set; } = default!;
-    public string Title { get; set; } = default!;
-    public long Totalquestions { get; set; } = default!;
+    public string PastPaperId { get; init; } = default!;
+    public string Title { get; init; } = default!;
+    public long Totalquestions { get; init; } = default!;
 }
