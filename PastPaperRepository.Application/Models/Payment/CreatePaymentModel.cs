f68510356901e6ff9c3b71ca1232f447345470e0
Sandip <srai68141@gmail.com>
2024-12-23 23:13:07 +0545

    feat: added payemnt model

---
+++ PastPaperRepository.Application/Models/Payment/CreatePaymentModel.cs
@@ -0,0 +1,9 @@
+﻿namespace PastPaperRepository.Application.Models.Payment;
+
+public class CreatePaymentModel
+{
+    public long UserId { get; init; }
+    public string ProductName { get; init; } = default!;
+    public long Price { get; init; } = default!;
+    public DateTime ValidUntil { get; init; }
+}
