ec74ec2628c0d70bf30fb96cf7b87bce524e4bb7
Sandip <srai68141@gmail.com>
2024-12-23 21:19:40 +0545

    feat: added contracts for create payment

---
+++ PastPaperRepository.Contracts/Requests/Payments/CreatePaymentRequest.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.InteropServices.JavaScript;
+
+namespace PastPaperRepository.Contracts.Requests.Payments;
+
+public class CreatePaymentRequest
+{
+    public long UserId { get; init; }
+    public string ProductName { get; init; } = default!;
+    public long Price { get; init; } = default!;
+    public DateTime ValidUntil { get; init; }
+}
