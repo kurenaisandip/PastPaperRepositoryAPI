9b00ce36db727462f5a23375717f4143e2f2525b
Sandip <srai68141@gmail.com>
2024-12-25 21:07:39 +0545

    feat; added request contract for subject and category

---
+++ PastPaperRepository.Contracts/Requests/EducationalEntities/CreateCategoriesRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace PastPaperRepository.Contracts.Requests.EducationalEntities;
+
+public class CreateCategoriesRequest
+{
+    public string CategoryName { get; init; }
+    public string Description { get; init; }
+}
---
+++ PastPaperRepository.Contracts/Requests/EducationalEntities/CreateSubjectRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace PastPaperRepository.Contracts.Requests.EducationalEntities;
+
+public class CreateSubjectRequest
+{
+    public string SubjectName { get; init; }
+    public string Description { get; init; }
+}
