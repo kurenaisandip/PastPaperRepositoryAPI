a4f6f9a91ea049cbff55da98ee6a82f8cd9d23d4
Sandip <srai68141@gmail.com>
2024-12-30 21:40:14 +0545

    fet: added open telemetry in console

--- PastPaperRepository.API/Program.cs
+++ PastPaperRepository.API/Program.cs
@@ -3,6 +3,7 @@ using Asp.Versioning;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
+using OpenTelemetry.Logs;
 using PastPaperRepository.API;
 using PastPaperRepository.API.Auth;
 using PastPaperRepository.API.Health;
@@ -14,6 +15,9 @@ using Stripe;
 using Swashbuckle.AspNetCore.SwaggerGen;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Logging.ClearProviders();
+builder.Logging.AddOpenTelemetry(x => x.AddConsoleExporter());
 
 // Add services to the container
 builder.Services.AddApiVersioning(x =>
@@ -92,6 +96,46 @@ builder.Services.AddSingleton<MailgunEmailSender>(sp => new MailgunEmailSender(
     builder.Configuration["Mailgun:Domain"]!
 ));
 
+SentrySdk.Init(options =>
+{
+    // A Sentry Data Source Name (DSN) is required.
+    // See https://docs.sentry.io/product/sentry-basics/dsn-explainer/
+    // You can set it in the SENTRY_DSN environment variable, or you can set it in code here.
+    options.Dsn = "https://5135dc7f165984f95071655459d3c59f@o4508546203713536.ingest.us.sentry.io/4508547039166464";
+
+    // When debug is enabled, the Sentry client will emit detailed debugging information to the console.
+    // This might be helpful, or might interfere with the normal operation of your application.
+    // We enable it here for demonstration purposes when first trying Sentry.
+    // You shouldn't do this in your applications unless you're troubleshooting issues with Sentry.
+    options.Debug = true;
+
+    // This option is recommended. It enables Sentry's "Release Health" feature.
+    options.AutoSessionTracking = true;
+
+    // Set TracesSampleRate to 1.0 to capture 100%
+    // of transactions for tracing.
+    // We recommend adjusting this value in production.
+    options.TracesSampleRate = 1.0;
+
+    // Sample rate for profiling, applied on top of othe TracesSampleRate,
+    // e.g. 0.2 means we want to profile 20 % of the captured transactions.
+    // We recommend adjusting this value in production.
+    options.ProfilesSampleRate = 1.0;
+    // Requires NuGet package: Sentry.Profiling
+    // Note: By default, the profiler is initialized asynchronously. This can
+    // be tuned by passing a desired initialization timeout to the constructor.
+    options.AddIntegration(new ProfilingIntegration(
+        // During startup, wait up to 500ms to profile the app startup code.
+        // This could make launching the app a bit slower so comment it out if you
+        // prefer profiling to start asynchronously
+        TimeSpan.FromMilliseconds(500)
+    ));
+});
+
+SentrySdk.CaptureMessage("Something went wrong");
+
+
+
 
 var app = builder.Build();
 
@@ -130,3 +174,5 @@ var dbInitializer = app.Services.GetRequiredService<DbInitalizer>();
 await dbInitializer.InitializeAsync();
 
 app.Run();
+
+
