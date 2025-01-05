3df1674978c6903e95898389964acca796f54bd3
Sandip <srai68141@gmail.com>
2025-01-05 12:55:38 +0545

    feat: added request contract for user data

---
+++ PastPaperRepository.Contracts/Requests/SendUserDataRequest.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PastPaperRepository.Contracts.Requests;
+
+public class SendUserDataRequest
+{
+    [Required(ErrorMessage = "InstitutionName is required.")]
+    public string? InstitutionName { get; init; }
+
+    [Required(ErrorMessage = "Grade is required.")]
+    public string? Grade { get; init; }
+
+    [Required(ErrorMessage = "Semester is required.")]
+    public string? Semester { get; init; }
+
+    [Required(ErrorMessage = "PhoneNumber is required.")]
+    [Range(1000000000, 9999999999, ErrorMessage = "PhoneNumber must be a 10-digit number.")]
+    public int PhoneNumber { get; init; }
+
+    [Required(ErrorMessage = "AcademicBackground is required.")]
+     public string? AcademicBackground { get; init; }
+
+}
