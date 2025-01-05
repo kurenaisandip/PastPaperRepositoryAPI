e1340551c71fce44b3b6dfac1ee006f36df1e674
Sandip <srai68141@gmail.com>
2025-01-05 12:55:15 +0545

    feat: added model LoggedInUserDetails

---
+++ PastPaperRepository.Application/Models/LoggedInUserDetails.cs
@@ -0,0 +1,11 @@
+﻿namespace PastPaperRepository.Application.Models;
+
+public class LoggedInUserDetails
+{
+    public string? InstitutionName { get; set; }
+    public string? Grade { get; set; }
+    public string? Semester { get; set; }
+    public int PhoneNumber { get; set; }
+    public string? AcademicBackground { get; set; }
+    
+}
