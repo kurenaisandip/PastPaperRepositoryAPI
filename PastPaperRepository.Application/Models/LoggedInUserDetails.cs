ee8d003a674729337a798b64b777cf29a72afaec
Sandip <srai68141@gmail.com>
2025-01-08 21:34:42 +0545

    refactor: changed the type of grade from string to int

--- PastPaperRepository.Application/Models/LoggedInUserDetails.cs
+++ PastPaperRepository.Application/Models/LoggedInUserDetails.cs
@@ -4,7 +4,7 @@ public class LoggedInUserDetails
 {
     public int UserId { get; set; }
     public string? InstitutionName { get; set; }
-    public string? Grade { get; set; }
+    public int? Grade { get; set; }
     public string? Semester { get; set; }
     public string PhoneNumber { get; set; }
     public string? AcademicBackground { get; set; }
