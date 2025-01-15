220071038ae94892780ffd9eaeeb1edf2f9aad8c
Sandip <srai68141@gmail.com>
2025-01-15 20:47:39 +0545

    feat: added model for DynamicPastPaperModal

---
+++ PastPaperRepository.Application/Models/DynamicPastPaperModal.cs
@@ -0,0 +1,9 @@
+﻿namespace PastPaperRepository.Application.Models;
+
+public class DynamicPastPaperModal
+{
+    public string PastPaperId { get; set; }
+    public string Title { get; set; }
+    public string Year { get; set; }
+    public string ExamBoard { get; set; }
+}
