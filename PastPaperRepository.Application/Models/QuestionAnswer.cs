26296342ac353b021366e6b7894fe0694fcd6779
Sandip <srai68141@gmail.com>
2025-01-18 23:20:44 +0545

    feat: added new model question answer for get past paper request

---
+++ PastPaperRepository.Application/Models/QuestionAnswer.cs
@@ -0,0 +1,9 @@
+﻿namespace PastPaperRepository.Application.Models;
+
+public class QuestionAnswer
+{
+    public int id { get; set; }
+    public string question { get; set; }
+    public string answer { get; set; }
+    
+}
